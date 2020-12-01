5e41b53f49350ff05872d9b6f95f30faea4d98b2
Chase Robbins <chaserobbins@me.com>
2020-12-01 13:25:11 -0800

    realtime full query

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,9 +6,13 @@ db.settings({
   timestampsInSnapshots: true
 });
 
-db.collection("messages").doc("test")
-    .onSnapshot(function(doc) {
-        console.log("Current data: ", doc.data());
+db.collection("messages")
+    .onSnapshot(function(querySnapshot) {
+        var cities = [];
+        querySnapshot.forEach(function(doc) {
+            cities.push(doc.data().value);
+        });
+        console.log("Current cities in CA: ", cities.join(", "));
     });
 
 const Form = ({setInputText, inputText}) => {
